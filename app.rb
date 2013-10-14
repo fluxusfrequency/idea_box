b7e28ebf96191d2b41a6428c99d7a6da90c4d333
Ben Lewis <bennlewis@gmail.com>
2013-10-13 20:54:25 -0600

    added CRUD functionality to ideas, iter 2 complete

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require './idea.rb'
 
 class IdeaBoxApp < Sinatra::Base
 
+  set :method_override, true
+
   configure :development do
     register Sinatra::Reloader
   end
@@ -10,9 +12,28 @@ class IdeaBoxApp < Sinatra::Base
     erb :index, locals: {ideas: Idea.all}
   end
 
+  get '/:id/edit' do |id|
+    idea = Idea.find(id.to_i)
+    erb :edit, locals: {id: id, idea: idea}
+  end
+
+  put '/:id' do |id|
+    data = {
+      :title => params['idea_title'],
+      :description => params['idea_description']
+    }
+    Idea.update(id.to_i, data)
+    redirect '/'
+  end
+
   post '/' do
     idea = Idea.new(params['idea_title'], params['idea_description'])
     idea.save
+    redirect '/'
+  end
+
+  delete '/:id' do |id|
+    Idea.delete(id.to_i)
     redirect '/'
   end
 
