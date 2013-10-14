957b9b32176094e25b8e2a54f114f1b316eb17a4
Ben Lewis <bennlewis@gmail.com>
2013-10-13 22:10:07 -0600

    completed iter_4

--- lib/idea_box/idea_store.rb
+++ lib/idea_box/idea_store.rb
@@ -2,13 +2,20 @@ require 'yaml/store'
 
 class IdeaStore
   def self.database
+    return @database if @database
     @database ||= YAML::Store.new('db/ideabox')
+    @database.transaction do
+      @database['ideas'] || []
+    end
+    @database
   end
 
   def self.all
-    raw_ideas.map do |data|
-      Idea.new(data)
+    ideas = []
+    raw_ideas.each_with_index do |data, i|
+      ideas << Idea.new(data.merge("id" => i))
     end
+    ideas
   end
 
   def self.raw_ideas
@@ -25,7 +32,7 @@ class IdeaStore
 
   def self.find(id)
     raw_idea = find_raw_idea(id)
-    Idea.new(raw_idea)
+    Idea.new(raw_idea.merge("id" => id))
   end
 
   def self.find_raw_idea(id)
@@ -42,7 +49,6 @@ class IdeaStore
 
   def self.create(attributes)
     database.transaction do
-      database['ideas'] ||= []
       database['ideas'] << attributes
     end
   end
