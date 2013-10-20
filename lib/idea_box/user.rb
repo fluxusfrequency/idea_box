04839974685aa39e4e848ac8be475a5aab1e7555
Ben Lewis <bennlewis@gmail.com>
2013-10-20 14:10:48 -0600

    created user attrs, setting up default hash

--- lib/idea_box/user.rb
+++ lib/idea_box/user.rb
@@ -1,9 +1,25 @@
+require 'digest/md5'
+
 class User
   attr_reader :id, :created_at
   attr_accessor :username, :password, :email, :portfolios
 
-  def initialize(attributes)
-    
+  def initialize(attributes={})
+    # attributes = default_user.merge(attributes)
+    @id          = attributes["id"]
+    @username    = attributes["username"]
+    @password    = Digest::MD5.hexdigest(attributes["password"])
+    @email       = attributes["email"]
+    @portfolios  = attributes["portfolios"]
+    @created_at  = attributes["created_at"]
   end
+
+  def default_user
+    {
+      "id"        => 1 # next_id
+      "username"  => "new user"
+      "password"  => Digest::MD5.hexdigest("password")
+      "email"     =>
+    }
 
 end
