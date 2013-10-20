e945d889f967583f0bbcb2945cdc30a211617b8c
Ben Lewis <bennlewis@gmail.com>
2013-10-20 14:14:23 -0600

    on second thought, i did setup user default attrs

--- lib/idea_box/user.rb
+++ lib/idea_box/user.rb
@@ -5,13 +5,24 @@ class User
   attr_accessor :username, :password, :email, :portfolios
 
   def initialize(attributes={})
-    # attributes = default_user.merge(attributes)
+    attributes = default_user.merge(attributes)
     @id          = attributes["id"]
     @username    = attributes["username"]
     @password    = Digest::MD5.hexdigest(attributes["password"])
     @email       = attributes["email"]
     @portfolios  = attributes["portfolios"]
-    @created_at  = attributes["created_at"] || Time.now.to_s
+    @created_at  = attributes["created_at"]
+  end
+
+  def default_user
+    {
+      "id"         => 1, # next_id
+      "username"   => "new user",
+      "password"   => Digest::MD5.hexdigest("password"),
+      "email"      => "johndoe@example.com",
+      "portfolios" => { 1 => 'work'},
+      "created_at" => Time.now.to_s
+    }
   end
 
 end
